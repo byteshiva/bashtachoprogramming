721492f90979be0f5cb19394fe9629802bae69b3
Siva <byteshiva@users.noreply.github.com>
2018-05-06 13:48:29 +0530

    tail log files with numbers
    1. tail log files with numbers 
    2. get numbers from input file and retrieve images using curl from xkcd
    3. use seq instead of number from files.
    4. redirect retrieve files to an output file to avoid repeated calls to server

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,13 @@
 !#/bin.bash
 
+# pipe seq of # from 1 to 105 to get xkcd images 
 seq 1 105 | xargs -i bash -c "curl -s https://xkcd.com/"{}"/info.0.json | jq -r '.img'"
+
+# Get number from input_file.txt for retrieving images from xkcd
 awk '{for(i=1; i<=NF; i++) system("curl -s https://xkcd.com/"$i"/info.0.json | jq -r '.img' ")}' input_file.txt
+
+# tail log files with numbers
+tail -f /var/log/foo.log | less -N
+
+# Redirect retrieve files to an output file to avoid repeated calls to server
+seq 1 1989 | xargs -i bash -c "curl -s https://xkcd.com/"{}"/info.0.json | jq -r '.img'" >> xkcdimgs.list
